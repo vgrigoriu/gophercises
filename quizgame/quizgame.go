dfb64232559bde139fae5f5607eb4c45bd0a25bb
Victor Grigoriu <grigoriu@adobe.com>
2019-11-16 08:06:13 +0200

    hello, world

---
+++ quizgame/quizgame.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("bau")
+	// read lines from csv
+	// parse lines into question & response
+	// display question
+	// read response
+	// count right / wrong answers
+	// display results
+}
